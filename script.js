595621f7508b45a0c1d7cc472925527bf685454b
codeWithEdwin <social@kinloch.me>
2019-05-18 22:24:59 -0500

    Created a function that grab a random element from myNumbers array

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+function random_item(myNumbers)
+{
+  
+return myNumbers[Math.floor(Math.random()*myNumbers.length)];
+     
+}
+
+
+/* 
+I need to create an Event trigger that runs a function to assign the user's input to a varibale
+When the user enter a number
+*/
+
+// document.getElementById("enteredNumber").addEventListener(ty)
+
+// function assignNumber() {
+//     var input = document.getElementById("enteredNumber").value;
+// }
+
+
+
+/*
+
+*/
