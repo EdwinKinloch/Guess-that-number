1134cc0a50a830436891a6510b70c393cc7b8930
codeWithEdwin <social@kinloch.me>
2019-05-19 00:04:55 -0500

    Added a function called compareNumbers that will compare user entered num and random num.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+/*This is function pick a random element from the myNumbers array. 
+Just call random_item(myNumbers) to get random number.
+*/
 function random_item(myNumbers)
 {
   
@@ -13,11 +17,17 @@ I need to create an Event trigger that runs a function to assign the user's inpu
 When the user enter a number
 */
 
-// document.getElementById("enteredNumber").addEventListener(ty)
+document.getElementById("submitNumber").addEventListener("click", compareNumbers);
 
-// function assignNumber() {
-//     var input = document.getElementById("enteredNumber").value;
-// }
+function compareNumbers() {
+    let enteredNumber = Number(document.getElementById("enteredNumber").value);
+    let randomNumber = random_item(myNumbers);
+    if (randomNumber === enteredNumber) {
+        document.getElementById("display").textContent="Correct";
+    }else{
+        document.getElementById("display").textContent= "Incorrect";
+    }
+}
 
 
 
